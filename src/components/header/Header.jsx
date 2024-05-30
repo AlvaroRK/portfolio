93292ce13df819c0115f3adf750060bf49ef26e9
AlvaroRK <adriano.rk06@gmail.com>
2024-05-30 20:19:59 -0300

    header y footer agregado

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,14 @@
+import NavBar from "./NavBar";
+import './header.css';
+
+const Header = () => {
+	return (
+		<header>
+			<h2>
+				<a href="#">ARK/</a>
+			</h2>
+			<NavBar />
+		</header>
+	);
+};
+export default Header;
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
 import './index.css'
+import Header from './components/header/Header'
+import Footer from './components/footer/Footer'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <Header />
+    <Footer/>
   </React.StrictMode>,
 )
