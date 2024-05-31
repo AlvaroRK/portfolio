8f61b71b658724c804c641bb18fed4064d4f8f47
AlvaroRK <adriano.rk06@gmail.com>
2024-05-31 20:24:26 -0300

    home creado

---
+++ src/components/buttons/CV.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import "./cv.css"
+
+const CV = () => {
+  return (
+    <a download={"#"}>
+      <button className='cvBtn'>Descargar CV</button>
+    </a>
+  )
+}
+
+export default CV
--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,16 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import './index.css'
-import Header from './components/header/Header'
-import Footer from './components/footer/Footer'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import Header from "./components/header/Header";
+import Footer from "./components/footer/Footer";
+import Home from "./components/pages/home/Home";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Header />
-    <Footer/>
-  </React.StrictMode>,
-)
+    <main>
+      <Home />
+    </main>
+    <Footer />
+  </React.StrictMode>
+);
