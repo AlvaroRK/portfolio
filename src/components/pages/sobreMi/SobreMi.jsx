b9a36dfca3241e47df32ecf2e9b3b9ce50a3172f
AlvaroRK <adriano.rk06@gmail.com>
2024-06-02 02:08:09 -0300

    sobre mí creado

---
+++ src/components/pages/sobreMi/SobreMi.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import "./sobreMi.css";
+
+const SobreMi = () => {
+  return (
+    <section className="sobreMi">
+      <h2>SOBRE MÍ</h2>
+      <div className="sobreMiContainer">
+        <img src="src\assets\imgNoche.jpeg" alt="img" />
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo
+          laudantium corrupti praesentium, sed enim alias voluptatibus sapiente
+          molestias ut. Nam cupiditate repudiandae omnis incidunt vero
+          accusantium debitis provident facere aspernatur in. Veniam, voluptatum
+          eius. Enim fugit ratione iste dicta fuga.Lorem ipsum dolor sit amet
+          consectetur adipisicing elit. Illo laudantium corrupti praesentium,
+          sed enim alias voluptatibus sapiente molestias ut. Nam cupiditate
+          repudiandae omnis incidunt vero accusantium debitis provident facere
+          aspernatur in. Veniam, voluptatum eius. Enim fugit ratione iste dicta
+          fuga.
+        </p>
+      </div>
+    </section>
+  );
+};
+
+export default SobreMi;
--- src/main.jsx
+++ src/main.jsx
@@ -4,12 +4,14 @@ import "./index.css";
 import Header from "./components/header/Header";
 import Footer from "./components/footer/Footer";
 import Home from "./components/pages/home/Home";
+import SobreMi from "./components/pages/sobreMi/SobreMi";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Header />
     <main>
       <Home />
+      <SobreMi />
     </main>
     <Footer />
   </React.StrictMode>
